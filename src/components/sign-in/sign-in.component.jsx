4b51fe1e509814b267565ed544f04742874491f1
dostongulmatov <58285468+dostongulmatov@users.noreply.github.com>
2021-01-31 09:29:38 +0500

    added some new features

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -41,7 +41,7 @@ class SignIn extends React.Component {
           <FormInput type="email" name="email" label="email" value={this.state.email} handleChange={this.handleChange} required/>
           <FormInput type="password" name="password" label="password" value={this.state.password} handleChange={this.handleChange}  required/>
           <CustomButton type="submit"> Sign in</CustomButton>
-          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>{' '}Sign In with Google{' '}</CustomButton>
+          <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>{' '}Sign In with Google{' '}</CustomButton>
 
         </form>
 
